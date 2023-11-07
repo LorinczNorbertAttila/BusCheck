53c8fa95c68cd5fab2907c4774487e09f4eb0187
Lőrincz Norbert Attila <norbertattila11@gmail.com>
2023-11-07 12:45:44 +0200

    Added Google map as a dependency

--- buscheck/ios/Runner/AppDelegate.swift
+++ buscheck/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBLcJo-3ra4SbuKH3duaZyGelw3Zjkui7A")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
