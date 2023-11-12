1da321375201f7568004d1ce162e0edc39941f4f
Lőrincz Norbert Attila <norbertattila11@gmail.com>
2023-11-12 23:40:38 +0200

    Regiser and sign in page created

--- buscheck/lib/screens/menu.dart
+++ buscheck/lib/screens/menu.dart
@@ -1,4 +1,6 @@
 import 'package:advanced_icon/advanced_icon.dart';
+import 'package:buscheck/screens/registration_page.dart';
+import 'package:buscheck/services/auth.dart';
 import 'package:buscheck/theme/theme_provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -11,9 +13,12 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    if (FirebaseAuth.instance.currentUser != null) {
+    if (FirebaseAuth.instance.currentUser != null &&
+        FirebaseAuth.instance.currentUser!.isAnonymous != true) {
       return Drawer(
         child: ListView(
           children: [
@@ -22,6 +27,78 @@ class _MenuState extends State<Menu> {
                     FirebaseAuth.instance.currentUser!.displayName.toString()),
                 accountEmail:
                     Text(FirebaseAuth.instance.currentUser!.email.toString())),
+            ElevatedButton(
+              onPressed: () async {
+                await _auth.signOut();
+              },
+              child: const Text(
+                'Sign out',
+                style: TextStyle(fontFamily: 'LilitaOne', fontSize: 22),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                AdvancedIcon(
+                  icon: Icons.light_mode,
+                  secondaryIcon: Icons.dark_mode,
+                  state: Theme.of(context).brightness == Brightness.dark
+                      ? AdvancedIconState.secondary
+                      : AdvancedIconState.primary,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                const Text(
+                  'Change Theme:',
+                  style: TextStyle(fontFamily: 'LilitaOne', fontSize: 22),
+                ),
+                const ChangeTeheme(),
+              ],
+            )
+          ],
+        ),
+      );
+    } else if (FirebaseAuth.instance.currentUser?.isAnonymous == true) {
+      return Drawer(
+        child: ListView(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.account_circle_outlined,
+                  size: 30,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                const Text(
+                  'Guest account',
+                  style: TextStyle(fontFamily: 'LilitaOne', fontSize: 22),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Text(
+                  'Do you want an account?',
+                  style: TextStyle(fontFamily: 'LilitaOne', fontSize: 22),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.cyan[400],
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (cosntext) => const Registration()));
+                  },
+                  child: const Text(
+                    'Register',
+                    style: TextStyle(fontFamily: 'LilitaOne', fontSize: 22),
+                  ),
+                ),
+              ],
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -48,17 +125,21 @@ class _MenuState extends State<Menu> {
       child: ListView(
         children: [
           ListTile(
-            leading: Icon(
-              Icons.account_circle_outlined,
-              size: 30,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-            title: const Text(
-              'Sign in/Registartion',
-              style: TextStyle(fontFamily: 'LilitaOne', fontSize: 22),
-            ),
-            onTap: null,
-          ),
+              leading: Icon(
+                Icons.account_circle_outlined,
+                size: 30,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              title: const Text(
+                'Sign in / Registartion',
+                style: TextStyle(fontFamily: 'LilitaOne', fontSize: 22),
+              ),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (cosntext) => const Registration()));
+              }),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
