a0bb02e3e103b6558492a8c2952d23c37c213921
Lőrincz Norbert Attila <norbertattila11@gmail.com>
2023-12-04 13:34:34 +0200

    No rating bar for buses when user is not signed in

--- buscheck/lib/screens/bus_page/bus_list.dart
+++ buscheck/lib/screens/bus_page/bus_list.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
@@ -35,88 +36,159 @@ class _BusListState extends State<BusList> {
               builder: (BuildContext context,
                   AsyncSnapshot<QuerySnapshot> snapshot) {
                 if (snapshot.hasData) {
-                  final snap = snapshot.data!.docs;
-                  return ListView.builder(
-                    shrinkWrap: true,
-                    primary: false,
-                    itemCount: snap.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        height: 70,
-                        width: double.infinity,
-                        margin: EdgeInsets.only(bottom: height * 0.015),
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).scaffoldBackgroundColor,
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.black,
-                              offset: Offset(2, 2),
-                              blurRadius: 10,
-                            ),
-                          ],
-                        ),
-                        child: Stack(
-                          children: [
-                            Row(
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.only(left: width * 0.03),
-                                  child: Icon(
-                                    Icons.directions_bus,
-                                    color:
-                                        Theme.of(context).colorScheme.primary,
-                                  ),
-                                ),
-                                Container(
-                                  alignment: Alignment.centerLeft,
-                                  child: Text(
-                                    " - ${snap[index].id}",
-                                    style: TextStyle(
+                  if (FirebaseAuth.instance.currentUser != null) {
+                    final snap = snapshot.data!.docs;
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      primary: false,
+                      itemCount: snap.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          height: 70,
+                          width: double.infinity,
+                          margin: EdgeInsets.only(bottom: height * 0.015),
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).scaffoldBackgroundColor,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.black,
+                                offset: Offset(2, 2),
+                                blurRadius: 10,
+                              ),
+                            ],
+                          ),
+                          child: Stack(
+                            children: [
+                              Row(
+                                children: [
+                                  Padding(
+                                    padding:
+                                        EdgeInsets.only(left: width * 0.03),
+                                    child: Icon(
+                                      Icons.directions_bus,
                                       color:
                                           Theme.of(context).colorScheme.primary,
-                                      fontWeight: FontWeight.bold,
                                     ),
                                   ),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.only(left: width * 0.025),
-                                  alignment: Alignment.centerLeft,
-                                  child: Text(
-                                    "Rating: ${snap[index]['Rating']}/5.00",
-                                    style: TextStyle(
-                                      color: Colors.cyan[400],
-                                      fontWeight: FontWeight.bold,
+                                  Container(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                      " - ${snap[index].id}",
+                                      style: TextStyle(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .primary,
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
-                            ),
-                            Container(
-                              margin: EdgeInsets.only(right: width * 0.03),
-                              alignment: Alignment.centerRight,
-                              child: RatingBar.builder(
-                                initialRating: 0,
-                                minRating: 1,
-                                direction: Axis.horizontal,
-                                allowHalfRating: true,
-                                itemCount: 5,
-                                itemSize: 26,
-                                glowColor: Colors.cyan[400],
-                                itemPadding: EdgeInsets.symmetric(
-                                    horizontal: width * 0.003),
-                                itemBuilder: (context, _) => const Icon(
-                                  Icons.star,
-                                  color: Colors.amber,
-                                ),
-                                onRatingUpdate: (rating) {},
+                                  Container(
+                                    margin:
+                                        EdgeInsets.only(left: width * 0.025),
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                      "Rating: ${snap[index]['Rating']}/5.00",
+                                      style: TextStyle(
+                                        color: Colors.cyan[400],
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  );
+                              Container(
+                                margin: EdgeInsets.only(right: width * 0.03),
+                                alignment: Alignment.centerRight,
+                                child: RatingBar.builder(
+                                  initialRating: 0,
+                                  minRating: 1,
+                                  direction: Axis.horizontal,
+                                  allowHalfRating: true,
+                                  itemCount: 5,
+                                  itemSize: 26,
+                                  glowColor: Colors.cyan[400],
+                                  itemPadding: EdgeInsets.symmetric(
+                                      horizontal: width * 0.003),
+                                  itemBuilder: (context, _) => const Icon(
+                                    Icons.star,
+                                    color: Colors.amber,
+                                  ),
+                                  onRatingUpdate: (rating) {},
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  } else {
+                    final snap = snapshot.data!.docs;
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      primary: false,
+                      itemCount: snap.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          height: 70,
+                          width: double.infinity,
+                          margin: EdgeInsets.only(bottom: height * 0.015),
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).scaffoldBackgroundColor,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.black,
+                                offset: Offset(2, 2),
+                                blurRadius: 10,
+                              ),
+                            ],
+                          ),
+                          child: Stack(
+                            children: [
+                              Row(
+                                children: [
+                                  Padding(
+                                    padding:
+                                        EdgeInsets.only(left: width * 0.03),
+                                    child: Icon(
+                                      Icons.directions_bus,
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                    ),
+                                  ),
+                                  Container(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                      " - ${snap[index].id}",
+                                      style: TextStyle(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .primary,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    margin:
+                                        EdgeInsets.only(left: width * 0.025),
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                      "Rating: ${snap[index]['Rating']}/5.00",
+                                      style: TextStyle(
+                                        color: Colors.cyan[400],
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  }
                 } else {
                   return const SizedBox();
                 }
